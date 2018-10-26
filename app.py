6d8c124d348f922b262d38126d94ec79584e4e63
Arkady Parakhin <ne-imperator@yandex.com>
2018-10-26 17:23:34 +0000

    added static folder with login form

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__, static_folder='/data/static')
+
+
+def show_the_login_form():
+    return app.send_static_file('form.html')
+
+
+def do_the_login(username, password):
+    return username + ' ' + password
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        username = request.form.get('username', '')
+        password = request.form.get('password', '')
+        return do_the_login(username, password)
+    else:
+        return show_the_login_form()
+
+
+if __name__ == '__main__':
+    app.run()
+
